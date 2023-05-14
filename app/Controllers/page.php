11b3d8e30b30d37ae6a24ce85ade02845c615a49
Indy <indyputri44@gmail.com>
2023-05-14 18:17:28 +0700

    lab7web

---
+++ app/Controllers/page.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        return view('about', [
+            'title' => 'Halaman About',
+            'content' => 'Ini adalah halaman about yang menjelaskan tentang isi halaman ini.'
+        ]);
+    }
+
+    public function contact()
+    {
+        echo "Ini halaman Contact";
+    }
+
+    public function faqs()
+    {
+        echo "Ini halaman FAQ";
+    }
+
+    public function tos()
+    {
+        echo "ini halaman term of services";
+    }
+
+    public function Artikel()
+    {
+        echo "ini halaman artikel";
+    }
+}
